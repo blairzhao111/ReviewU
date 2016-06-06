2cda6f0d1a22aa4670136164971314062a345253
Junwei Zhao <blairzhao111@gmail.com>
2016-06-06 18:43:22 -0400

    Add mongodb config file, set db-related log functions and set dbURI for dev and live

---
+++ app_server/models/db.js
@@ -0,0 +1,57 @@
+
+var mongoose = require('mongoose');
+var connection = mongoose.connection;
+var shutdown;
+
+//set mongodb URI based on environment, by default it's development.
+var dbURI = 'mongodb://localhost/ReviewU';
+
+//if on live environment, use heroku's MONGOLAB_URI environment variable to set connection.
+if(process.env.NODE_ENV === 'production'){
+	dbURI = process.env.MONGOLAB_URI;
+}
+
+mongoose.connect(dbURI);
+
+//Listen for Mongoose connection events and output statuses to console
+connection.on('connected', function(){
+	console.log("Mongoose connected to " + dbURI);
+});
+
+connection.on('error', function(err){
+	console.error("Mongoose connection error: " + err);
+});
+
+connection.on('disconnected', function(){
+	console.log('Mongoose is disconnected.');
+});
+
+shutdown = function(msg, callback){
+	connection.close(function(){
+		console.log("Mongoose is disconnected through " + msg);
+		callback();
+	});
+};
+
+//for nodemon restart
+/*process.once('SIGUSR2', function(){
+	shutdown('nodemon restart', function(){
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});*/
+
+//for app termination.
+process.on('SIGINT', function(){
+	shutdown("local app termination", function(){
+		process.exit(0);
+	});
+});
+
+//for heroku app termination. 
+process.on('SIGTERM', function(){
+	shutdown("heroku app termination", function(){
+		process.exit(0);
+	});
+});
+
+require('./locations.js');
