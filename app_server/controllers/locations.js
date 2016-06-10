5092d65b7cf9f761b93b24733b17d1967d013baa
Junwei Zhao <blairzhao111@gmail.com>
2016-06-10 17:36:08 -0400

    finsih homelist controller reading data from api

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,44 +1,85 @@
+var request = require('request'),
+    apiOptions = {
+      server: 'http://localhost:3000'
+    };
 
+if(process.env.NODE_ENV === 'production'){
+  apiOptions.server = '';
+}
+
+//process the distance data in 
+var formatDistance = function(locations){
+  if(!locations || locations.length === 0){return;}
+  locations.forEach(function(location){
+    var distance, unit,
+        locationDistance = parseFloat(location.distance);
+    if(locationDistance<1000){
+      distance = parseInt(locationDistance, 10);
+      unit = 'm';
+    }else{
+      distance = (locationDistance/1000).toFixed(1);
+      unit = 'km';
+    }
+    location.distance = distance + unit;    
+  });
+  return locations;
+};
+
+//function for rendering homelist view
+var renderListPage = function(req, res, locations){
+  var message;
+  if(!(locations instanceof Array)){
+    message = 'API lookup error';
+    locations = [];
+  }else{
+    if(!locations.length){
+      message = 'No places found nearby!'
+    }
+  }
+  res.render('location-list', { 
+    title: 'ReviewU - Share your reviews with us and find others',
+    pageHeader: {
+      title: 'ReviewU',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+    locations: locations,
+    message: message
+  });
+};
+
+
+/**
+*  Export section
+**/
 /* Get 'Home' Page*/
 module.exports.homelist = function (req, res) {
-  res.render('location-list', { 
-  	title: 'Loc8r - find a place to work with wifi',
-  	pageHeader: {
-  		title: 'ReviewU',
-  		strapline: 'Find places to work with wifi near you!'
-  	},
-    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-  	locations: [{
-  		name: 'Starcups',
-  		address: '125 High Street, Reading, RG6 1PS',
-  		rating: 3,
-  		distance: '100m',
-  		facilities: ['Hot drinks', 'Food', 'Premium wifi']
-  	}, {
-  		name: 'Cafe Hero',
-  		address: '125 High Street, Reading, RG6 1PS',
-  		rating: 4,
-  		distance: '150m',
-  		facilities: ['Beer', 'Food', 'Premium wifi']
-  	}, {
-  		name: 'Burger Queen',
-  		address: '125 High Street, Reading, RG6 1PS',
-  		rating: 4,
-  		distance: '230m',
-  		facilities: ['Burger', 'Food', 'Premium wifi']
-  	}, {
-  		name: 'Pizza Feast',
-  		address: '125 High Street, Reading, RG6 1PS',
-  		rating: 5,
-  		distance: '300m',
-  		facilities: ['Pizza', 'Food', 'Premium wifi']
-  	}, {
-   		name: 'Grill Heaven',
-  		address: '125 High Street, Reading, RG6 1PS',
-  		rating: 4,
-  		distance: '350m',
-  		facilities: ['Grill', 'Drinks', 'Premium wifi']
-  	}]
+  var path = '/api/locations',
+      requestOptions = {
+          url: apiOptions.server + path,
+          method: 'GET',
+          json: {},
+          qs: {
+            lng: req.query.lng || '-78.8148184',
+            lat: req.query.lat || '43.0056247',
+            maxdis: req.query.maxdis || null,
+            num: req.query.num || null
+          }
+      };  
+
+  request(requestOptions, function(err, response, body){
+      if(err){
+        console.log(err);
+      }else if(response.statusCode !== 200){
+        console.log(response.statusCode);
+      }else{
+        var locations;
+        locations = JSON.parse(body);
+        if(locations.length){
+          locations = formatDistance(locations); 
+        }
+        renderListPage(req, res, locations);
+      }  
   });
 };
 
