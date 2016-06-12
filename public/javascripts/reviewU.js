e6a9eed48c78a85b87ebf8921af59bc01d00f1fd
Junwei Zhao <blairzhao111@gmail.com>
2016-06-11 21:26:47 -0400

    increase font size and border effect when user mouse enter each list area

--- public/javascripts/reviewU.js
+++ public/javascripts/reviewU.js
@@ -1,13 +1,26 @@
 
 (function($){
-	var $area = $('div.area');
-	$('p a.detail').hide();
-	$area
-		.on('mouseenter', function(){
-			$(this).find('a.detail').fadeToggle(300);
-		})
-		.on('mouseleave', function(){
-			$(this).find('a.detail').fadeToggle(300);
-		});
-
+	$(document).ready(function(){
+		var $area = $('div.area');
+		$('p a.detail').hide();
+		$area
+			.on('mouseenter', function(){
+				$(this)
+					.css({
+						'border': '4px solid #CCC',
+						'font-size': '130%'
+					})
+					.find('a.detail')
+						.fadeToggle(300);
+			})
+			.on('mouseleave', function(){
+				$(this)
+					.css({
+						'border': 'none',
+						'font-size': '100%'
+					})
+					.find('a.detail')
+						.fadeToggle(300);
+			});		
+	});
 })(jQuery);
