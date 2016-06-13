338e76021597a98e6aca611c151c4230737ce697
Junwei Zhao <blairzhao111@gmail.com>
2016-06-12 20:44:07 -0400

    add favicon into application

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//middelwares
+app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 //defining routing
 app.use('/', routes);
